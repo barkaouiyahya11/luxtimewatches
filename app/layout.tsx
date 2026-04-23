488fbb57bf78d32dd6ef6e68c8e3f5f172a7995c
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-04-24 00:26:45 +0100

    Fix hydration mismatch error by adding suppressHydrationWarning to html and body tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html
       lang="fr"
       className={`${playfair.variable} ${inter.variable} ${cairo.variable} ${allura.variable}`}
+      suppressHydrationWarning
     >
       <head>
         <link
@@ -62,7 +63,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
       </head>
-      <body className="font-sans bg-white">
+      <body className="font-sans bg-white" suppressHydrationWarning>
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
         <Script id="gtag-init" strategy="afterInteractive">{`
           window.dataLayer = window.dataLayer || [];
