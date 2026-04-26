84c9878fbda5acad60f9c0419b2153608f48ff9a
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-04-26 23:27:31 +0100

    Remove margin between Hero and AnnouncementBar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,6 @@ export default function Hero() {
         maxHeight: '700px',
         background:
           "linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.3)), url('https://i.ibb.co/wvW0v0m/Gemini-Generated-Image-swd0bkswd0bkswd0.png') no-repeat center center / cover",
-        marginBottom: '30px',
       }}
     >
       <div className="flex flex-col md:flex-row gap-3 px-4 w-full max-w-3xl pb-8 z-10">
