3e7df8bd149d11b92f235a03ec514d5f21529cf5
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-01 22:55:06 +0100

    feat: add new product

--- data/products.ts
+++ data/products.ts
@@ -172,4 +172,20 @@ export const products: Product[] = [
       'https://i.ibb.co/mFPDwqXL/Whats-App-Image-2026-03-27-at-20-25-57-2.jpg',
     ],
   },
+  {
+    id: 101,
+    sku: 'htl212',
+    cat: 'homme',
+    name: 'ROLEX',
+    price: 180,
+    originalPrice: 230,
+    stock: 5,
+    rating: 4.9,
+    reviews: 0,
+    gridImg: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1777670578/luxtim/cdtdfhnvlqd3iybgmps5.jpg',
+    detailImgs: [
+      'https://res.cloudinary.com/dannr2e0c/image/upload/v1777670578/luxtim/cdtdfhnvlqd3iybgmps5.jpg',
+    ],
+    hot: true,
+  },
 ]
