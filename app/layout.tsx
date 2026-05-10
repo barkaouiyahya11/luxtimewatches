faaad8d82d4ee353460a17b2d919886cc5c254b5
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-10 20:56:46 +0100

    Add watch emoji favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,9 @@ export const metadata: Metadata = {
     type: 'website',
   },
   twitter: { card: 'summary' },
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⌚</text></svg>",
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
