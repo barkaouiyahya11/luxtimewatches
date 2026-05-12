d61d7db378b2b5060f45d37d3d8c43366e0507e7
barkaouiyahya11 <barkaouiyahya11@gmail.com>
2026-05-12 15:04:30 +0100

    Update GoldBanner images via admin dashboard

--- components/GoldBanner.tsx
+++ components/GoldBanner.tsx
@@ -8,12 +8,12 @@ const BANNER_IMAGES = [
     alt: 'GUSHKIN Collection 1',
   },
   {
-    src: 'https://i.ibb.co/dwDPGFXf/Whats-App-Image-2026-03-23-at-18-02-06-1.jpg',
+    src: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1778594659/luxtim/no6n6ecvcz9azmiprzjd.jpg',
     alt: 'GUSHKIN Luxe',
   },
   {
     src: 'https://i.ibb.co/DfJh70n6/Gemini-Generated-Image-rdy4m6rdy4m6rdy4-1.png',
-    alt: 'GUSHKIN Collection 2',
+    alt: 'GUSHKIN Prestige',
   },
 ]
 
