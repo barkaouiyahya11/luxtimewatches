bd0d3dbf1cd712f7428ddd20eec7efc62b698e00
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-16 23:47:18 +0100

    Fix: URL Apps Script avec notification email

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxHTPgJff2HYjcDSsVsyDe6aGLmWxIsx01ZRboVvUZim2EMW2FdHUnw5--i5C0OGos9Wg/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx-FWI9B6vbwPesIcxdgowLM_-oOTCH0Y6VCjUetamRrtE16A8YRIZij-pBFKyIKk-FFg/exec'
