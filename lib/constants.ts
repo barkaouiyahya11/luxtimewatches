72a4ae01c451a7e150b41883100a8b635367b74a
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-16 18:38:02 +0100

    Fix: URL finale Apps Script structure simplifiée

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwBNwESpNC2NQH7hgy_me7p9_CZgziPIIAXWaQiUyeQmWkIMe2-EfFiGHhyGiojkYJr/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyA3OjeEBWeBVlNG9iows3Efuy2-tbhbl4_fmBRV5ZLrFyb74JaIZF-ZGZbRt-fHlTE/exec'
