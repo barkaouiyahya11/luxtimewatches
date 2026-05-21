053fb363d714c9b19b5c10c2d56c3e6f2c106346
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-21 11:07:51 +0100

    Update Apps Script URL - full row coloring

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0VICuKitFsYA0KTsSNVjSqj0IZmjYBELq0Lv6W66_abjQKN7KOGI81sNmxQiRpVwSUw/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwXecG5sSI8Ejmj8hhuP-_Fi6d_D47Iz4NSm0WIsU8qQvVA8THbACgSSdaMGZ9y38vuqQ/exec'
