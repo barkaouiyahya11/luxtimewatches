2828b7b4faa068a28c077d75c200dd5ad1d98aa5
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-24 19:50:28 +0100

    Update Google Orders Script URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwW1OGTNq3_kM-vsETPMbqgj37g5_pRNcnBPrghyIGIAEy2X-TfeBEiclcYgRZqagXEGA/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwQqMqLIT9MyKgxfKPQEfCKNvet1QUayDf8IBNZi7TJ1BHNDx_aOxZ9FOe5Nnfngs8PjA/exec'
