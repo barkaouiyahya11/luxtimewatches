3eec998e855492da7e4d9a52197dba52021ccb43
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-26 18:35:30 +0100

    Update Apps Script URL with Telegram notifications

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzkQsAm0eu2DkwGJiK_lOVEKFt3QeMZ_2myT_r9SC6w2bCpk7rM4UuWZcesUtftQeEx/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyD75hJJlSVTl6ZsbrlFcxmpoqMOtJRP8oXup2CNYoSrJNl7zACnHOHBLwhz-RyV8Sz/exec'
