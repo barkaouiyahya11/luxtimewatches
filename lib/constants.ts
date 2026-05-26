d9973386370d6360d98c18299ee4e7e7b2a74ece
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-26 19:04:47 +0100

    Update Apps Script URL - fix field names for gestion app

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyyvIvngxCqralOaCEmSOnXplo-8AgP4C9pB2bBBoWL8EvZqdjkzRSDGQIAVSBUQXcN/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzP1CuvrQNfUz_E3X9TT4AZzZCo9Ht4rQVKaD4MRMkZpl6JBHEDoy1iyHw9Bn8uhU5u/exec'
