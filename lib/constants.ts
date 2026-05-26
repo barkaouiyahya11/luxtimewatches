c0141027221f42ba8904701d3d354ab937ab07dc
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-05-26 18:53:08 +0100

    Update Apps Script URL - Telegram in doPost

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const GOOGLE_SCRIPT_URL = ''
 
 // URL Google Apps Script pour recevoir les commandes (orders)
 // Collez ici l'URL de déploiement de votre Apps Script après l'avoir créé
-export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx02RGQR7G-O6iZ9TZBxuwfH1AewFCagF1Wp15WcW_FFtAAnRYLsnKvFwHdCIaR2BqT/exec'
+export const GOOGLE_ORDERS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyyvIvngxCqralOaCEmSOnXplo-8AgP4C9pB2bBBoWL8EvZqdjkzRSDGQIAVSBUQXcN/exec'
