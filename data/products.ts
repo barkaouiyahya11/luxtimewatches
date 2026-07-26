f599a5eae8f31efa5d1bfb3d9c7847a17dcb0258
barkaouiyahya11 <barkaouiyahya11@gmail.com>
2026-07-26 14:17:28 +0100

    Update product id:131 via admin dashboard

--- data/products.ts
+++ data/products.ts
@@ -242,7 +242,7 @@ export const products: Product[] = [
     stock: 0,
     rating: 4.9,
     reviews: 0,
-    gridImg: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1781119991/luxtim/uimfdblwsdjr09pveu1h.jpg',
+    gridImg: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1785071841/luxtim/sf2hu37p55odogcnolmy.jpg',
     detailImgs: [
       'https://res.cloudinary.com/dannr2e0c/image/upload/v1780849803/luxtim/vdwz7gxinwslmagmyosj.jpg',
     ],
