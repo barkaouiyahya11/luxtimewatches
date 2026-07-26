83b9f32543b70b297eddecf8c94bc74dd7889f0b
barkaouiyahya11 <barkaouiyahya11@users.noreply.github.com>
2026-07-26 14:36:26 +0100

    Supprimer GMT-Master II (id 156) et Arabic Dial (id 158)

--- data/products.ts
+++ data/products.ts
@@ -565,21 +565,6 @@ export const products: Product[] = [
     ],
   },
   {
-    id: 156,
-    sku: 'GSH-H-BS-13',
-    cat: 'homme',
-    name: 'ROLEX GMT-MASTER II HOMME BOITE SIMPLE',
-    price: 230,
-    originalPrice: 270,
-    stock: 0,
-    rating: 4.9,
-    reviews: 0,
-    gridImg: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1785068970/luxtim/qjoxcf676w5fofipvyoq.jpg',
-    detailImgs: [
-      'https://res.cloudinary.com/dannr2e0c/image/upload/v1781122407/luxtim/zu0xsv0irewe5ztpznot.jpg',
-    ],
-  },
-  {
     id: 157,
     sku: 'GSH-H-BS-14',
     cat: 'homme',
@@ -592,21 +577,6 @@ export const products: Product[] = [
     gridImg: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1780844290/luxtim/frgdrkebmbj22qyua66d.jpg',
     detailImgs: [
       'https://res.cloudinary.com/dannr2e0c/image/upload/v1780844348/luxtim/vzumhks4u98r2ibxfav1.jpg',
-    ],
-  },
-  {
-    id: 158,
-    sku: 'GSH-H-BS-15',
-    cat: 'homme',
-    name: 'ROLEX ARABIC DIAL HOMME BOITE SIMPLE',
-    price: 230,
-    originalPrice: 250,
-    stock: 0,
-    rating: 4.9,
-    reviews: 0,
-    gridImg: 'https://res.cloudinary.com/dannr2e0c/image/upload/v1780848558/luxtim/obn0mn8dzoibjmvfupvh.jpg',
-    detailImgs: [
-      'https://res.cloudinary.com/dannr2e0c/image/upload/v1780848838/luxtim/x3zrkdjefgi7dxndzovh.jpg',
     ],
   },
   {
