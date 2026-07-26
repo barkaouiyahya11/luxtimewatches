0d0f4d47a50a5644b57fbe03de7fda2fd7dd418c
barkaouiyahya11 <barkaouiyahya11@gmail.com>
2026-07-26 13:22:09 +0100

    Update product id:133 via admin dashboard

--- data/products.ts
+++ data/products.ts
@@ -281,6 +281,7 @@ export const products: Product[] = [
     detailImgs: [
       'https://res.cloudinary.com/dannr2e0c/image/upload/v1785068331/luxtim/doyzuyx5ucuw7fqu5nhg.jpg',
     ],
+    imgScale: 1.1,
   },
   {
     id: 134,
